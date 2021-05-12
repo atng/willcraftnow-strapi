c42b9826795d4d452b1bfc7708efc7661cc9a68a
atng <tng.weiheng.alvin@gmail.com>
2021-05-12 19:07:26 +0800

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '46eec11f33f1767251e539dc887bc3ec'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6d95b90c-603b-4c45-a989-2cc110c2ada1'
+};
